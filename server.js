d5be17287e3566a215282eb752df7d3846268ed1
Dennis Wronka <reptiler@users.sourceforge.net>
2016-07-09 21:12:48 +0200

    Added command nearby

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Discord = require("discord.js");
 var config = require('./config.js');
 var edsm = require('./edsm.js');
 
-const VERSION = "FGEBot Version 0.3.2-JTJ3.2";
+const VERSION = "FGEBot Version 0.3.2-JTJ4";
 
 var FGEBot = new Discord.Client();
 
