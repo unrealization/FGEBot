e5bdb230c40dc2e47c64c44026f01a3d8ba8345c
Dennis Wronka <reptiler@users.sourceforge.net>
2016-07-19 01:21:59 +0200

    Version bump

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var config = require('./config.js');
 var edsm = require('./edsm.js');
 var edmaterializer = require("./edmaterializer.js");
 
-const VERSION = "FGEBot Version 0.3.2-JTJ10";
+const VERSION = "FGEBot Version 0.3.2-JTJ11";
 
 var FGEBot = new Discord.Client();
 
