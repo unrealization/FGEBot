25239ac82fb3c89ae10934b10beb030f87b6e5d7
Dennis Wronka <reptiler@users.sourceforge.net>
2016-11-15 15:58:50 +0100

    Implemented a queue for bulk role changes.

--- modules/ccn.js
+++ modules/ccn.js
@@ -3,7 +3,8 @@ var botFunctions = require("../bot_functions.js");
 
 //
 const VERSION = "0.9.4";
-const DISCORDID = 209372315673165825;
+//const DISCORDID = 209372315673165825;
+const DISCORDID = 212228086849077248;
 
 var defaultModuleConfig = {
 	"CCN_PROXIMITY_ROLE": "",
@@ -11,6 +12,37 @@ var defaultModuleConfig = {
 	"CCN_EDSM_ID": "",
 	"CCN_EDSM_APIKEY": "",
 };
+
+//
+function processRoleQueue(bot, roleQueue) {
+	function roleHandler(error) {
+		if (error) {
+			console.log("Waiting");
+			roleQueue.unshift(roleAction);
+			setTimeout(processRoleQueue(bot, roleQueue), 10000);
+		} else {
+			processRoleQueue(bot, roleQueue);
+		}
+	}
+
+	console.log("Entries in role queue: " + roleQueue.length);
+
+	if (roleQueue.length == 0) {
+		return;
+	}
+
+	var roleAction = roleQueue.shift();
+
+	if (roleAction.action == "add") {
+		console.log("Adding role " + roleAction.role.name + " to user " + roleAction.user.name);
+		bot.addMemberToRole(roleAction.user, roleAction.role, roleHandler);
+	}
+
+	if (roleAction.action == "remove") {
+		console.log("Removing role " + roleAction.role.name + " from user " + roleAction.user.name);
+		bot.removeMemberFromRole(roleAction.user, roleAction.role, roleHandler);
+	}
+}
 
 //
 function getApiObject(server) {
@@ -47,6 +79,7 @@ function proximityCheck(args, bot, msg) {
 		var serverMembers = msg.server.members;
 		var proximityRoleMembers = msg.server.usersWithRole(proximityRole);
 		var output = "";
+		var roleQueue = [];
 
 		for (var index=0; index<serverMembers.length; index++) {
 			var edsm = botFunctions.getModule(msg.server, "edsm");
@@ -68,12 +101,24 @@ function proximityCheck(args, bot, msg) {
 
 			if (dataIndex > -1 && roleIndex == -1) {
 				output += serverMembers[index].name + " has arrived in Colonia." + "\n";
-				bot.addMemberToRole(serverMembers[index], proximityRole, roleHandler);
+				//bot.addMemberToRole(serverMembers[index], proximityRole, roleHandler);
+				var roleAction = {
+					"action": "add",
+					"user": serverMembers[index],
+					"role": proximityRole
+				};
+				roleQueue.push(roleAction);
 			}
 
 			if (dataIndex == -1 && roleIndex > -1) {
 				output += serverMembers[index].name + " is no longer in Colonia." + "\n";
-				bot.removeMemberFromRole(serverMembers[index], proximityRole, roleHandler);
+				//bot.removeMemberFromRole(serverMembers[index], proximityRole, roleHandler);
+				var roleAction = {
+					"action": "remove",
+					"user": serverMembers[index],
+					"role": proximityRole
+				};
+				roleQueue.push(roleAction);
 			}
 		}
 
@@ -82,6 +127,7 @@ function proximityCheck(args, bot, msg) {
 			return;
 		}
 
+		processRoleQueue(bot, roleQueue);
 		botFunctions.sendMessage(bot, msg.channel, output);
 	}
 
@@ -116,11 +162,66 @@ function proximityCheck(args, bot, msg) {
 	ccn_edsm.getColoniaCommanders(edsmId, edsmApiKey, 200, callback);
 }
 
+function queueTest(args, bot, msg) {
+	var roleQueue = [];
+	var role = botFunctions.getRole(msg.server, 215456960248676354);
+
+	if (!role) {
+		botFunctions.sendMessage(bot, msg.channel, "Error");
+		return;
+	}
+
+	var roleActionAdd = {
+		"action": "add",
+		"user": msg.author,
+		"role": role
+	};
+
+	var roleActionRemove = {
+		"action": "remove",
+		"user": msg.author,
+		"role": role
+	};
+
+	roleQueue.push(roleActionAdd);
+	roleQueue.push(roleActionRemove);
+
+	roleQueue.push(roleActionAdd);
+	roleQueue.push(roleActionRemove);
+
+	roleQueue.push(roleActionAdd);
+	roleQueue.push(roleActionRemove);
+
+	roleQueue.push(roleActionAdd);
+	roleQueue.push(roleActionRemove);
+
+	roleQueue.push(roleActionAdd);
+	roleQueue.push(roleActionRemove);
+
+	roleQueue.push(roleActionAdd);
+	roleQueue.push(roleActionRemove);
+
+	roleQueue.push(roleActionAdd);
+	roleQueue.push(roleActionRemove);
+
+	roleQueue.push(roleActionAdd);
+	//roleQueue.push(roleActionRemove);
+
+	processRoleQueue(bot, roleQueue);
+}
+
 //
 var commands = {
 	"proximityCheck": {
 		help: "Automatically assign roles based on Commander location",
 		process: proximityCheck,
+		permissions: [
+			"administrator"
+		]
+	},
+	"queueTest": {
+		help: "Test the role queue",
+		process: queueTest,
 		permissions: [
 			"administrator"
 		]
