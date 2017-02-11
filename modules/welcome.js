61a0c45a15d22c7980b5756840b5a30e590bd9cf
Dennis Wronka <reptiler@users.sourceforge.net>
2017-02-11 19:28:04 +0100

    Welcome moved over to proper functions.

--- modules/welcome.js
+++ modules/welcome.js
@@ -1,6 +1,6 @@
 var botFunctions = require("../bot_functions.js");
 
-const VERSION = "1.2";
+const VERSION = "1.3";
 
 var defaultModuleConfig = {
 	"WELCOME_CHANNEL": "",
@@ -24,26 +24,64 @@ function newMemberHandler(bot, server, user) {
 	return false;
 }
 
+function getWelcomeChannel(args, bot, msg)
+{
+	var welcomeChannel = botFunctions.getConfigValue(msg.server, "WELCOME_CHANNEL");
+
+	if (!welcomeChannel)
+	{
+		botFunctions.sendMessage(bot, msg.channel, "No welcome channel has been set.");
+		return;
+	}
+
+	var serverChannel = botFunctions.getChannel(msg.server, welcomeChannel);
+
+	if (!serverChannel)
+	{
+		botFunctions.sendMessage(bot, msg.channel, "A channel is set, but it does not exist on this server.");
+		return;
+	}
+
+	botFunctions.sendMessage(bot, msg.channel, "The welcome channel is: " + serverChannel.name);
+}
+
+function setWelcomeChannel(args, bot, msg)
+{
+	var channel = botFunctions.compileArgs(args);
+
+	if (!channel)
+	{
+		if (!botFunctions.setConfigValue(msg.server, "WELCOME_CHANNEL", ""))
+		{
+			botFunctions.sendMessage(bot, msg.channel, "There was a problem storing the setting.");
+			return;
+		}
+
+		botFunctions.sendMessage(bot, msg.channel, "The channel for welcome messages has been cleared.");
+		return;
+	}
+
+	var serverChannel = botFunctions.getChannelByName(msg.server, channel);
+
+	if (!serverChannel)
+	{
+		botFunctions.sendMessage(bot, msg.channel, "Cannot find the channel " + channel);
+		return;
+	}
+
+	if (!botFunctions.setConfigValue(msg.server, "WELCOME_CHANNEL", serverChannel.id))
+	{
+		botFunctions.sendMessage(bot, msg.channel, "There was a problem storing the setting.");
+		return;
+	}
+
+	botFunctions.sendMessage(bot, msg.channel, "The channel for welcome messages has been set to: " + serverChannel.name);
+}
+
 var commands = {
 	"getWelcomeChannel": {
 		help: "Shows the currently set channel for welcome messages for new users.",
-		process: function(args, bot, msg) {
-			var welcomeChannel = botFunctions.getConfigValue(msg.server, "WELCOME_CHANNEL");
-
-			if (!welcomeChannel) {
-				botFunctions.sendMessage(bot, msg.channel, "No welcome channel has been set.");
-				return;
-			}
-
-			var serverChannel = botFunctions.getChannel(msg.server, welcomeChannel);
-
-			if (!serverChannel) {
-				botFunctions.sendMessage(bot, msg.channel, "A channel is set, but it does not exist on this server.");
-				return;
-			}
-
-			botFunctions.sendMessage(bot, msg.channel, "The welcome channel is: " + serverChannel.name);
-		},
+		process: getWelcomeChannel,
 		permissions: [
 			"administrator"
 		]
@@ -51,33 +89,7 @@ var commands = {
 	"setWelcomeChannel": {
 		usage: "[channel]",
 		help: "Set a channel, or none, in which the bot will post a welcome message for new users on this Discord.",
-		process: function(args, bot, msg) {
-			var channel = botFunctions.compileArgs(args);
-
-			if (!channel) {
-				if (!botFunctions.setConfigValue(msg.server, "WELCOME_CHANNEL", "")) {
-					botFunctions.sendMessage(bot, msg.channel, "There was a problem storing the setting.");
-					return;
-				}
-
-				botFunctions.sendMessage(bot, msg.channel, "The channel for welcome messages has been cleared.");
-				return;
-			}
-
-			var serverChannel = botFunctions.getChannelByName(msg.server, channel);
-
-			if (!serverChannel) {
-				botFunctions.sendMessage(bot, msg.channel, "Cannot find the channel " + channel);
-				return;
-			}
-
-			if (!botFunctions.setConfigValue(msg.server, "WELCOME_CHANNEL", serverChannel.id)) {
-				botFunctions.sendMessage(bot, msg.channel, "There was a problem storing the setting.");
-				return;
-			}
-
-			botFunctions.sendMessage(bot, msg.channel, "The channel for welcome messages has been set to: " + serverChannel.name);
-		},
+		process: setWelcomeChannel,
 		permissions: [
 			"administrator"
 		]
